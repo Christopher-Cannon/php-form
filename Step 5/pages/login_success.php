2f64d1d9f7f92089dabed5424441e23ee7387e47
Christopher-Cannon <Christopher-Cannon@users.noreply.github.com>
2017-08-16 13:46:32 +0100

    Added step 5

---
+++ Step 5/pages/login_success.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+	<head>
+		<title>Login success</title>
+
+		<meta charset="UTF-8">
+
+		<link rel="stylesheet" type="text/css" href="../css/style.css" />
+	</head>
+
+	<body>
+		<div class="main-wrapper">
+      <h1>Login successful!</h1>
+		</div>
+	</body>
+</html>
---
+++ Step 5/pages/reg_success.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="en">
+	<head>
+		<title>Registration success</title>
+
+		<meta charset="UTF-8">
+
+		<link rel="stylesheet" type="text/css" href="../css/style.css" />
+	</head>
+
+	<body>
+		<div class="main-wrapper">
+      <h1>Registration successful!</h1>
+
+      <a href="../index.php">Return to login</a>
+		</div>
+	</body>
+</html>

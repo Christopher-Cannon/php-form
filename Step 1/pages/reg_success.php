ac63f3e466b840e9a8fc60569b4f90a86b7bb4e5
Christopher-Cannon <Christopher-Cannon@users.noreply.github.com>
2017-08-16 09:51:46 +0100

    Created two success pages

--- Step 1/pages/reg_success.php
+++ Step 1/pages/reg_success.php
@@ -2,16 +2,18 @@
 
 <html lang="en">
 	<head>
-		<title>Registration and login</title>
+		<title>Registration success</title>
 
 		<meta charset="UTF-8">
 
-		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="stylesheet" type="text/css" href="../css/style.css" />
 	</head>
 
 	<body>
 		<div class="main-wrapper">
       <h1>Registration successful!</h1>
+
+      <a href="../index.php">Return to login</a>
 		</div>
 	</body>
 </html>
